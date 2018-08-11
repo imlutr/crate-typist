68c099c98ffb03ac2c10021ef3a91915a2edacdd
Luca1152 <t.luca1152@gmail.com>
2018-08-11 15:35:29 +0300

    Added some code to test the GameMap, Crate and Turret classes

--- core/src/ro/luca1152/typing/screens/PlayScreen.java
+++ core/src/ro/luca1152/typing/screens/PlayScreen.java
@@ -1,22 +1,63 @@
 package ro.luca1152.typing.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
 import ro.luca1152.typing.TypingGame;
+import ro.luca1152.typing.actors.Crate;
+import ro.luca1152.typing.actors.GameMap;
 
 public class PlayScreen extends ScreenAdapter {
     private TypingGame game;
     private Stage stage;
+    private GameMap currentMap;
 
     public PlayScreen(TypingGame game) {
         this.game = game;
         stage = new Stage(game.viewport, game.batch);
-        stage.addActor(new Image(new Texture("background.png")));
+
+        // Map
+        currentMap = new GameMap("map1");
+        stage.addActor(currentMap);
+
+        // Crates
+        Crate leftCrate = new Crate(-1, 8, true);
+        leftCrate.addAction(sequence(
+                delay(2f),
+                moveBy(576f, 0f, 4.5f),
+                moveBy(0f, -384f, 3f)));
+        stage.addActor(leftCrate);
+        Crate rightCrate = new Crate(10, 1, true);
+        rightCrate.addAction(sequence(
+                delay(2f),
+                moveBy(-576f, 0f, 4.5f),
+                moveBy(0f, 384f, 3f)));
+        stage.addActor(rightCrate);
+
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+                if (keycode == Keys.A || keycode == Keys.LEFT)
+                    currentMap.turret.rotatingLeft = true;
+                if (keycode == Keys.D || keycode == Keys.RIGHT)
+                    currentMap.turret.rotatingRight = true;
+                return true;
+            }
+
+            @Override
+            public boolean keyUp(int keycode) {
+                if (keycode == Keys.A || keycode == Keys.LEFT)
+                    currentMap.turret.rotatingLeft = false;
+                if (keycode == Keys.D || keycode == Keys.RIGHT)
+                    currentMap.turret.rotatingRight = false;
+                return true;
+            }
+        });
     }
 
     @Override
