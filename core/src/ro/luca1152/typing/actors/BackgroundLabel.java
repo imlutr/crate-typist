2f9da6b77221015749bdb3481ebf11ff4b86df6d
Luca1152 <t.luca1152@gmail.com>
2018-08-12 00:20:59 +0300

    Added the BackgroundLabel class, which lets you set the background of a label to a certain color.

---
+++ core/src/ro/luca1152/typing/actors/BackgroundLabel.java
@@ -0,0 +1,16 @@
+package ro.luca1152.typing.actors;
+
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+public class BackgroundLabel extends Label {
+    public BackgroundLabel(CharSequence text, LabelStyle labelStyle) {
+        super(text, labelStyle);
+        Pixmap labelBg = new Pixmap((int)getPrefWidth(), (int)getPrefHeight(), Pixmap.Format.RGBA8888);
+        labelBg.setColor(0f, 0f, 0f, 0.5f);
+        labelBg.fill();
+        getStyle().background = new Image(new Texture(labelBg)).getDrawable();
+    }
+}
