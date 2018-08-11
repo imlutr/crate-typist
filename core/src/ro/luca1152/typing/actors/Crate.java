d169575896e382fe887f54d95a74f690f3e92e92
Luca1152 <t.luca1152@gmail.com>
2018-08-12 00:25:26 +0300

    Added a label with a background to every crate representing a word. Also, now when the crate reaches the finish point it disappears.

--- core/src/ro/luca1152/typing/actors/Crate.java
+++ core/src/ro/luca1152/typing/actors/Crate.java
@@ -1,27 +1,78 @@
 package ro.luca1152.typing.actors;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 
 import ro.luca1152.typing.TypingGame;
 
 public class Crate extends Image {
-    public Crate(TypingGame game, float x, float y, boolean mapCoordinates) {
+    // Static because in Java the super() call has to be first
+    private static String words[] = {
+            "if", "ad", "by",
+            "for", "max", "mix",
+            "pair", "gift", "jump",
+            "white", "alien", "board",
+            "smooth", "strong", "barrel"};
+    private static String word;
+
+    private BackgroundLabel label;
+    private Table table;
+
+    private Vector2[] finishPoints;
+
+    public Crate(TypingGame game, float x, float y, boolean mapCoordinates, Vector2[] finishPoints) {
         super(randomTexture(game));
         if (mapCoordinates) {
             x *= 64;
             y *= 64;
         }
+        this.finishPoints = finishPoints;
         setPosition(x, y);
+        table = new Table();
+        table.setSize(getWidth(), getHeight());
+        table.setPosition(getX(), getY());
+        label = new BackgroundLabel(word, game.getLabelStyle());
+        table.add(label).size(label.getPrefWidth() + 4, label.getPrefHeight() + 3);
+        label.setAlignment(Align.center);
     }
 
     private static Texture randomTexture(TypingGame game) {
-        if (MathUtils.random(1, 2) == 1)
+        word = words[MathUtils.random(0, words.length - 1)];
+        if (word.length() <= 3)
             return game.getManager().get("textures/wooden_crate.png");
-        else
+        else if (word.length() <= 6)
             return game.getManager().get("textures/golden_crate.png");
+        return null;
+    }
+
+    @Override
+    protected void setStage(Stage stage) {
+        super.setStage(stage);
+        if (stage != null) {
+            stage.addActor(table);
+        }
+    }
+
+    @Override
+    protected void positionChanged() {
+        super.positionChanged();
+        if (table != null) {
+            table.setPosition(getX(), getY());
+            for (Vector2 finishPoint : finishPoints) {
+                if (getX() == finishPoint.x * 64 && getY() == finishPoint.y * 64) {
+                    remove();
+                    table.remove();
+                }
+            }
+        }
+    }
+
+    public String getWord() {
+        return word;
     }
 }
