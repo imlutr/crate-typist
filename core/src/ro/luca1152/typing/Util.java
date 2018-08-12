fd28a0245da39ef69724f22013cbef5a860193af
Luca1152 <t.luca1152@gmail.com>
2018-08-12 23:43:15 +0300

    Added an Util class for the angle between two objects.

---
+++ core/src/ro/luca1152/typing/Util.java
@@ -0,0 +1,16 @@
+package ro.luca1152.typing;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class Util {
+    public static float getAngleBetween(Actor a, Actor b) {
+        float angle = (float) Math.atan2((a.getY() + a.getOriginY()) - (b.getY() + b.getOriginY()), (a.getX() + a.getOriginX()) - (b.getX() + b.getOriginX()));
+        angle = (float) (angle * (180 / Math.PI));
+        if (angle < 0)
+            angle = 360 - (-angle);
+        angle += 90;
+        if (angle > 360)
+            angle = angle - 360;
+        return angle;
+    }
+}
