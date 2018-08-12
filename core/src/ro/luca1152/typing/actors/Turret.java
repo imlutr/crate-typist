d5100f971134d4b91df7bb145a006a877d3beece
Luca1152 <t.luca1152@gmail.com>
2018-08-12 22:25:11 +0300

    Made rotation of the turret smoother + solved bug where turret would rotate 360 for no reason.

--- core/src/ro/luca1152/typing/actors/Turret.java
+++ core/src/ro/luca1152/typing/actors/Turret.java
@@ -1,8 +1,9 @@
 package ro.luca1152.typing.actors;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import ro.luca1152.typing.TypingGame;
@@ -36,13 +37,19 @@ public class Turret extends Image {
     }
 
     private void rotateTo(Actor target) {
-        float angle = (float)Math.atan2((getY() + getOriginY()) - (target.getY() + target.getOriginY()),(getX() +  getOriginX()) - (target.getX() + target.getOriginX()));
-        angle = (float)(angle * (180/Math.PI));
-        if(angle < 0)
+        // Get the angle
+        float angle = (float) Math.atan2((getY() + getOriginY()) - (target.getY() + target.getOriginY()), (getX() + getOriginX()) - (target.getX() + target.getOriginX()));
+        angle = (float) (angle * (180 / Math.PI));
+        if (angle < 0)
             angle = 360 - (-angle);
         angle += 90;
-        if(angle > 360)
+        if (angle > 360)
             angle = angle - 360;
-        setRotation(angle);
+
+        float angleDiff = Math.abs(getRotation() - angle);
+        if (angleDiff > 1f && angleDiff <= 250f)
+            addAction(Actions.rotateTo(angle, 0.05f, Interpolation.exp5In));
+        else
+            setRotation(angle);
     }
 }
