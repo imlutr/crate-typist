dc3f3073dac8fffab959e79191b8da9f14fd6aa5
Luca1152 <t.luca1152@gmail.com>
2018-08-12 16:52:49 +0300

    Moved some logic from PlayScreen to GameMap.

--- core/src/ro/luca1152/typing/actors/GameMap.java
+++ core/src/ro/luca1152/typing/actors/GameMap.java
@@ -1,34 +1,119 @@
 package ro.luca1152.typing.actors;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 
 import ro.luca1152.typing.TypingGame;
 
-public class GameMap extends Image {
-    private int mapId;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.after;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
+
+public class GameMap extends Group {
+    private final TypingGame game;
+
+    // Map
+    private JsonValue currentMap;
+    private Vector2[] finishPoints;
+    private int id;
+
+    // Children
+    private Image mapImage;
     private Turret turret;
+    private Group crates;
+
+    private float crateTimer = 0f;
 
     public GameMap(TypingGame game, int mapId) {
-        super(game.getManager().get("maps/map" + mapId + ".png", Texture.class));
-        this.mapId = mapId;
+        this.game = game;
+        this.id = mapId;
+
+        mapImage = new Image(game.getManager().get("maps/map" + mapId + ".png", Texture.class));
+        addActor(mapImage);
+
         turret = new Turret(game, 284f, 284f);
-        turret.setZIndex(1); // Put the turret under the crates' label
+        addActor(turret);
+
+        crates = new Group();
+        addActor(crates);
+
+        JsonReader jsonReader = new JsonReader();
+        currentMap = jsonReader.parse(Gdx.files.internal("maps/maps.json")).get(id);
+        finishPoints = finishPoints(currentMap);
+    }
+
+    private Vector2[] finishPoints(JsonValue currentMap) {
+        int numOfFinishPoints = currentMap.getInt("numOfFinishPoints");
+        Vector2[] finishPoints = new Vector2[numOfFinishPoints];
+        for (int i = 0; i < numOfFinishPoints; i++) {
+            JsonValue arrayOfPoints = currentMap.get("finishPoints");
+            if (numOfFinishPoints == 1)
+                finishPoints[i] = new Vector2(arrayOfPoints.getInt("x"), arrayOfPoints.getInt("y"));
+            else
+                finishPoints[i] = new Vector2(arrayOfPoints.get(i).getInt("x"), arrayOfPoints.get(i).getInt("y"));
+        }
+        return finishPoints;
     }
 
     @Override
-    protected void setStage(Stage stage) {
-        super.setStage(stage);
-        if (stage != null)
-            getStage().addActor(turret);
+    public void act(float delta) {
+        super.act(delta);
+        spawnCrates(delta);
+        removeCratesAtFinishPoint();
+    }
+
+    private void spawnCrates(float delta) {
+        crateTimer -= delta;
+        if (crateTimer <= 0f) {
+            crateTimer = 1.5f;
+
+            int randomSpawn = MathUtils.random(0, currentMap.getInt("numOfSpawns") - 1);
+            Crate crate = newCrate(currentMap, randomSpawn);
+            crates.addActor(crate);
+            addMovementActionsTo(crate, randomSpawn, currentMap);
+        }
+    }
+
+    private void removeCratesAtFinishPoint() {
+        for (Actor crate: crates.getChildren()) {
+            for (Vector2 finishPoint : finishPoints) {
+                if (crate.getX() == finishPoint.x * 64 && crate.getY() == finishPoint.y * 64) {
+                    removeActor(crate);
+                    crate.remove();
+                }
+            }
+        }
+    }
+
+    private Crate newCrate(JsonValue currentMap, int randomSpawn) {
+        JsonValue spawn = currentMap.get("spawns").get(randomSpawn);
+        return new Crate(game, spawn.getInt("x"), spawn.getInt("y"));
+    }
+
+    private void addMovementActionsTo(Crate crate, int randomSpawn, JsonValue currentMap) {
+        JsonValue path = currentMap.get("spawns").get(randomSpawn).get("path");
+        for (JsonValue value : path)
+            crate.addAction(after(moveTo(value.getInt("x") * 64, value.getInt("y") * 64, value.getInt("distance") / 2f)));
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        mapImage.draw(batch, parentAlpha);
+        turret.draw(batch, parentAlpha);
+        for (Actor child: crates.getChildren())
+            ((Crate)child).getCrateImage().draw(batch, parentAlpha);
+        for (Actor child: crates.getChildren())
+            ((Crate)child).getLabel().draw(batch, parentAlpha);
     }
 
     public Turret getTurret() {
         return turret;
-    }
-
-    public int getId() {
-        return mapId;
     }
 }
--- core/src/ro/luca1152/typing/screens/PlayScreen.java
+++ core/src/ro/luca1152/typing/screens/PlayScreen.java
@@ -5,38 +5,22 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.JsonReader;
-import com.badlogic.gdx.utils.JsonValue;
 
 import ro.luca1152.typing.TypingGame;
-import ro.luca1152.typing.actors.Crate;
 import ro.luca1152.typing.actors.GameMap;
 
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.after;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
-
 public class PlayScreen extends ScreenAdapter {
-    private TypingGame game;
     private Stage stage;
 
     // Map
-    private JsonValue mapsJson; // the file "maps/maps.json"
     private GameMap map;
 
-    private float crateTimer = 0f;
-
-
     PlayScreen(TypingGame game) {
-        this.game = game;
         stage = new Stage(game.getViewport(), game.getBatch());
 
         map = new GameMap(game, 0);
         stage.addActor(map);
-        JsonReader jsonReader = new JsonReader();
-        mapsJson = jsonReader.parse(Gdx.files.internal("maps/maps.json"));
 
         setInputProcessor();
     }
@@ -45,6 +29,9 @@ public class PlayScreen extends ScreenAdapter {
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean keyDown(int keycode) {
+                if (keycode >= Keys.A && keycode <= Keys.Z) {
+                }
+
                 if (keycode == Keys.A || keycode == Keys.LEFT)
                     map.getTurret().rotatingLeft = true;
                 if (keycode == Keys.D || keycode == Keys.RIGHT)
@@ -65,51 +52,10 @@ public class PlayScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        update(delta);
+        stage.act(delta);
         Gdx.gl20.glClearColor(1f, 1f, 1f, 1f);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.draw();
-    }
-
-    private void update(float delta) {
-        stage.act(delta);
-        spawnCrates(delta);
-    }
-
-    private void spawnCrates(float delta) {
-        crateTimer -= delta;
-        if (crateTimer <= 0f) {
-            crateTimer = 1.5f;
-
-            JsonValue currentMap = mapsJson.get(map.getId());
-            int randomSpawn = MathUtils.random(0, currentMap.getInt("numOfSpawns") - 1);
-            addActionsTo(newCrate(currentMap, finishPoints(currentMap), randomSpawn), randomSpawn, currentMap);
-        }
-    }
-
-    private Vector2[] finishPoints(JsonValue currentMap) {
-        int numOfFinishPoints = currentMap.getInt("numOfFinishPoints");
-        Vector2[] finishPoints = new Vector2[numOfFinishPoints];
-        for (int i = 0; i < numOfFinishPoints; i++) {
-            JsonValue arrayOfPoints = currentMap.get("finishPoints");
-            if (numOfFinishPoints == 1)
-                finishPoints[i] = new Vector2(arrayOfPoints.getInt("x"), arrayOfPoints.getInt("y"));
-            else
-                finishPoints[i] = new Vector2(arrayOfPoints.get(i).getInt("x"), arrayOfPoints.get(i).getInt("y"));
-        }
-        return finishPoints;
-    }
-
-    private Crate newCrate(JsonValue currentMap, Vector2[] finishPoints, int randomSpawn) {
-        JsonValue spawn = currentMap.get("spawns").get(randomSpawn);
-        return new Crate(game, spawn.getInt("x"), spawn.getInt("y"), true, finishPoints);
-    }
-
-    private void addActionsTo(Crate crate, int randomSpawn, JsonValue currentMap) {
-        JsonValue path = currentMap.get("spawns").get(randomSpawn).get("path");
-        for (JsonValue value : path)
-            crate.addAction(after(moveTo(value.getInt("x") * 64, value.getInt("y") * 64, value.getInt("distance") / 2f)));
-        stage.addActor(crate);
     }
 
     @Override
