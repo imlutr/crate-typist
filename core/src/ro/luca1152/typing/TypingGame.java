8332665a090585dea844273993115e43a19e67aa
Luca1152 <t.luca1152@gmail.com>
2018-08-13 21:12:29 +0300

    Started adding highscores.

--- core/src/ro/luca1152/typing/TypingGame.java
+++ core/src/ro/luca1152/typing/TypingGame.java
@@ -1,6 +1,8 @@
 package ro.luca1152.typing;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -18,6 +20,7 @@ public class TypingGame extends Game {
     private AssetManager assetManager;
     private Label.LabelStyle labelStyle17bg, labelStyle30;
     private String[] wordList;
+    private Preferences preferences;
 
     public LoadingScreen loadingScreen;
     public MainMenuScreen mainMenuScreen;
@@ -29,6 +32,7 @@ public class TypingGame extends Game {
         viewport = new FitViewport(640f, 640f);
         assetManager = new AssetManager();
         wordList = new String[2261];
+        preferences = Gdx.app.getPreferences("Typing Game by Luca1152");
 
         loadingScreen = new LoadingScreen(this);
         mainMenuScreen = new MainMenuScreen(this);
@@ -71,5 +75,9 @@ public class TypingGame extends Game {
 
     public void setWordList(String[] wordList) {
         this.wordList = wordList;
+    }
+
+    public Preferences getPreferences() {
+        return preferences;
     }
 }
